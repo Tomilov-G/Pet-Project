a3c0b47118d032009365431bf06e4faba574419d
Tomilov-G <tomilovgleb591@gmail.com>
2025-01-23 00:49:16 +0700

    Фикс бага в скрипте slider

--- js/slider.js
+++ js/slider.js
@@ -3,12 +3,13 @@
 
 const needElement = document.body.querySelector(".banner")
 
-const image1 = needElement.style.backgroundImage = "url('../assets/img/bg.png')"
-const image2 = needElement.style.backgroundImage = "url('../assets/img/bgi2.jpg')"
-const image3 = needElement.style.backgroundImage = "url('../assets/img/bgi3.jpg')"
-const image4 = needElement.style.backgroundImage = "url('../assets/img/bgi4.jpg')"
+const arrOfImage = [
+    "url('../assets/img/bg.png')",
+    "url('../assets/img/bgi2.jpg')",
+    "url('../assets/img/bgi3.jpg')",
+    "url('../assets/img/bgi4.jpg')"
+]
 
-const arrOfImage = [image1,image2,image3,image4]
 let currentIndexOfImage = 0
 
 function nextImage(arr) {
