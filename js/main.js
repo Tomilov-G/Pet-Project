33ef20ac0c9c36f4d261b5e45ac3fbf05b29e32d
Tomilov-G <tomilovgleb591@gmail.com>
2025-01-22 19:12:46 +0700

    Добвлена проверка ввода данных в разделе

--- js/main.js
+++ js/main.js
@@ -4,11 +4,8 @@
 const needElement = document.body.querySelector(".banner")
 
 const image1 = needElement.style.backgroundImage = "url('../assets/img/bg.png')"
-
 const image2 = needElement.style.backgroundImage = "url('../assets/img/bgi2.jpg')"
-
 const image3 = needElement.style.backgroundImage = "url('../assets/img/bgi3.jpg')"
-
 const image4 = needElement.style.backgroundImage = "url('../assets/img/bgi4.jpg')"
 
 const arrOfImage = [image1,image2,image3,image4]
@@ -31,3 +28,41 @@ document.querySelector(".right-arrow").addEventListener("click", () => {
 document.querySelector(".left-arrow").addEventListener("click", () => {
     prevImage(arrOfImage);
 });
+
+
+
+// Отправка сообщенией
+
+
+const userName = document.body.querySelector(".user-name")
+const userTel = document.body.querySelector(".user-tel")
+const userMessage = document.body.querySelector("input[name = 'user-message']")
+
+const arrOfInput = [userName, userTel, userMessage]
+
+const sendMessageBtn = document.body.querySelector('.btn')
+const trueSendMessage = document.createElement('div')
+trueSendMessage.innerHTML = "Сообщение отправлено. Мы свяжемся с вами в ближайшее время."
+
+function checkInputFields(arr) {
+    for (let inputValue of arr) {
+        if (inputValue.value.trim() === '') {
+            inputValue.style.setProperty("--c", "red")
+        }
+    }
+    const hasEmptyFields = arr.some(element => element.value.trim() === '')
+    if(!hasEmptyFields) {
+        trueSendMessage.className = "trueValues"
+        sendMessageBtn.replaceWith(trueSendMessage)
+    }
+}
+
+sendMessageBtn.addEventListener("click", () => {
+    checkInputFields(arrOfInput)
+})
+
+
+
+
+
+
